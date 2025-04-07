d5d94ad0421f697b3a41e31e5b4e56f17b3026f1
xtension <47290097+xtnsn@users.noreply.github.com>
2025-04-07 23:07:01 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "ANIMMOMEN - sebuah momen yang tak terlupakan",
+  title: "sebuah momen yang tak terlupakan",
   description: "Portfolio website for ANIMMOMEN, a creative anime content creator",
   generator: "v0.dev",
 }
